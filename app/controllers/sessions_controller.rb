5d6e5f72e12cff8c5a017a3a0cdf21e6a93d5fee
Moneshsoni <sonimonesh115@gmail.com>
2023-08-14 15:50:25 +0530

    fix the issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,20 @@ class SessionsController < Devise::SessionsController
   end
 
   def create
+    binding.pry
     @user = User.find_by_email(params[:user][:email])
     if @user.approve?
-      self.resource = warden.authenticate!(auth_options)
-      sign_in @user
-    
-      redirect_to root_path
-
+      if @user != nil
+        self.resource = warden.authenticate!(auth_options)
+        sign_in @user
+        redirect_to root_path
+      end
     else
+      binding.pry
       flash[:alert] = 'User not approved Please contact Admin'
-    end
       redirect_to new_user_session_path
+    end
+    
   end
 
 end
