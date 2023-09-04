8bfee3d2f37bc4b7447269d00772094abf03d923
Moneshsoni <sonimonesh115@gmail.com>
2023-09-04 09:07:35 +0530

    add the contact validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["approve", "boolean", "city", "created_at", "email", "id", "name", "requirement", "surname", "updated_at"]
   end
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :city, presence: true
+  validates :requirement, presence: true 
 end
